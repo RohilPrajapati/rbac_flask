253b63d83b2ed953f07feeb09e2fec4a4a7eaed4
RohilPrajapati <rohilprajapati@gmail.com>
2026-02-21 00:26:57 +0545

    fix change init_db from old connection to connection pool

--- app/db.py
+++ app/db.py
@@ -1,4 +1,3 @@
-import psycopg2
 from app.config import DB_CONFIG
 from psycopg2 import pool
 from contextlib import contextmanager
@@ -29,96 +28,93 @@ def get_connection():
 
 
 def init_db():
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            # --- Create ENUM types ---
+            cursor.execute("""
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_enum') THEN
+                    CREATE TYPE gender_enum AS ENUM ('m','f','o');
+                END IF;
+            END$$;
+            """)
 
-    # --- Create ENUM types ---
-    cursor.execute("""
-    DO $$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_enum') THEN
-            CREATE TYPE gender_enum AS ENUM ('m','f','o');
-        END IF;
-    END$$;
-    """)
+            cursor.execute("""
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+                    CREATE TYPE user_role AS ENUM ('super_admin','artist_manager','artist');
+                END IF;
+            END$$;
+            """)
 
-    cursor.execute("""
-    DO $$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
-            CREATE TYPE user_role AS ENUM ('super_admin','artist_manager','artist');
-        END IF;
-    END$$;
-    """)
+            cursor.execute("""
+            DO $$
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'genre_enum') THEN
+                    CREATE TYPE genre_enum AS ENUM ('rnb','country','classic','rock','jazz');
+                END IF;
+            END$$;
+            """)
 
-    cursor.execute("""
-    DO $$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'genre_enum') THEN
-            CREATE TYPE genre_enum AS ENUM ('rnb','country','classic','rock','jazz');
-        END IF;
-    END$$;
-    """)
+            # --- Users table ---
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(255) NOT NULL,
+                last_name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                phone VARCHAR(20) NOT NULL,
+                dob DATE NOT NULL,
+                gender gender_enum NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW(),
+                role user_role NOT NULL
+            );
+            """)
+            print("Created User Table")
 
-    # --- Users table ---
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        phone VARCHAR(20) NOT NULL,
-        dob DATE NOT NULL,
-        gender gender_enum NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
-        role user_role NOT NULL
-    );
-    """)
-    print("Created User Table")
+            # --- Artist table ---
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS artist (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                dob DATE NOT NULL,
+                gender gender_enum NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                first_release_year INT NULL,
+                no_of_albums INT DEFAULT 0,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW(),
+                
+                user_id INT NULL,
+                CONSTRAINT fk_artist_user
+                    FOREIGN KEY (user_id)
+                    REFERENCES users(id)
+                    ON DELETE CASCADE
+            );
+            """)
+            print("Created Artist Table")
 
-    # --- Artist table ---
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS artist (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        dob DATE NOT NULL,
-        gender gender_enum NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        first_release_year INT NULL,
-        no_of_albums INT DEFAULT 0,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
-        
-        user_id INT NULL,
-        CONSTRAINT fk_artist_user
-            FOREIGN KEY (user_id)
-            REFERENCES users(id)
-            ON DELETE CASCADE
-    );
-    """)
-    print("Created Artist Table")
+            # --- Music table ---
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS music (
+                id SERIAL PRIMARY KEY,
+                artist_id INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                album_name VARCHAR(255) NOT NULL,
+                genre genre_enum NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW(),
+                CONSTRAINT fk_artist
+                    FOREIGN KEY (artist_id) 
+                    REFERENCES artist(id)
+                    ON DELETE CASCADE
+            );
+            """)
+            print("Created Music Table")
 
-    # --- Music table ---
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS music (
-        id SERIAL PRIMARY KEY,
-        artist_id int NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        album_name VARCHAR(255) NOT NULL,
-        genre genre_enum NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
-        CONSTRAINT fk_artist
-            FOREIGN KEY (artist_id) 
-            REFERENCES artist(id)
-            ON DELETE CASCADE
-    );
-    """)
-    print("Created Music Table")
-
-    conn.commit()
-    cursor.close()
-    conn.close()
+        conn.commit()
